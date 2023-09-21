6cce0934fb2d674289f227d6bc2963216af0a764
petruchevsky <m.berdichevsky.a@gmail.com>
2023-09-21 19:31:19 -0300

    probando openGraph

--- app/page.js
+++ app/page.js
@@ -1,11 +1,24 @@
 
 import Slider from "./components/slider/Slider"
 
+// export const metadata = {
+
+// 	 title: "Grupo 7 - Inicio",
+// 	 description: "Bienvenidos a Grupo 7",
+// 	 keywords: "inicio",
+// };
+
 export const metadata = {
-	 title: "Grupo 7 - Inicio",
-	 description: "Bienvenidos a Grupo 7",
-	 keywords: "inicio",
-};
+  openGraph: {
+    title: "Grupo 7 - Inicio",
+    description: "Bienvenidos a Grupo 7",
+    images: {
+      url: "https://res.cloudinary.com/dtqfrwjdm/image/upload/v1695334473/logo_radius_8870e0c763.png"
+    },
+    locale: 'es_CL',
+    type: 'website',
+  }
+}
 
 export default function Home() {
 
